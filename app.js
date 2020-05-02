072a3e4572471f5285100807db794d67ef5e6f44
Chirag Sinha <chirag.suman2025@gmail.com>
2020-05-03 01:25:16 +0530

    Added other files

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.get("/", function(req,res){
+    res.render("index");
+});
+app.get("/diet", function(req,res){
     res.render("diet");
+});
+app.get("/faq", function(req,res){
+    res.render("faq");
 });
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
